82981e914154d118375c400cadcb2e133860690d
William Thorsen <william@thorsen.io>
2019-01-16 08:56:39 -0800

    Grant access to ~/.ecs from within the container

--- activate-aws-cli.sh
+++ activate-aws-cli.sh
@@ -1,11 +1,15 @@
 #!/usr/bin/env bash
 
-# This script opens a shell in a container that enables the AWS CLI and the AWS ECS CLI.
+# This script opens a shell in which the AWS CLI and the AWS ECS CLI are enabled.
 
 docker container run \
   --interactive \
   --rm \
   --tty \
   --mount type=bind,source=$(PWD),target=/var/project \
+  --mount type=bind,source="${HOME}/.aws",target=/root/.aws \
+  --mount type=bind,source="${HOME}/.ecs",target=/root/.ecs \
+  --mount type=bind,source="${HOME}/.ssh",target=/root/.ssh \
   thorsenio/aws:latest \
   bash
+
